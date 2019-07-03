641f25edf2ae5fbce8a0495671406ecb4156c512
Strjahilev <evg.strjahilev@gmail.com>
2019-07-03 20:15:29 +0300

    ngModel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {MatButtonModule} from '@angular/material';
+import { CompaniesComponent } from './companies/companies.component';
+import { ProductsComponent } from './products/products.component';
+import {RouterModule} from '@angular/router';
+import {appRoutes} from './route';
+import { HomeComponent } from './home/home.component';
+import {HttpClientModule} from '@angular/common/http';
+import {HttpService} from './service';
+import {FormsModule} from '@angular/forms';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CompaniesComponent,
+    ProductsComponent,
+    HomeComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatButtonModule,
+    RouterModule.forRoot(appRoutes),
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [HttpService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
